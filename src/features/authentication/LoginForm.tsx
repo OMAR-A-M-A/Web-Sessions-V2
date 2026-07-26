1466c8f1a6817155f880cce76338cb32be982d5c
OMAR-A-M-A <oa5252842@gmail.com>
2026-07-26 16:18:41 +0300

    refactor the login form to new file and fix a bug

---
+++ src/features/authentication/LoginForm.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { useLogin } from "@/features/authentication/hooks/useLogin";
+
+import { Button } from "@/ui/button";
+import { Input } from "@/ui/input";
+import { Label } from "@/ui/label";
+import { MiniSpinner } from "@/ui/MiniSpinner";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("oa5252842@gmail.com");
+  const [password, setPassword] = useState("omar011");
+
+  const { login, isPending } = useLogin();
+
+  function handleSubmit(e: React.SubmitEvent) {
+    e.preventDefault();
+    if (!email || !password) return;
+
+    login(
+      { email, password },
+      {
+        onSettled: () => setPassword(""),
+      },
+    );
+  }
+  return (
+    <form onSubmit={handleSubmit} className="space-y-6">
+      <div className="space-y-2">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          type="email"
+          placeholder="admin@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={isPending}
+          required
+        />
+      </div>
+
+      <div className="space-y-2">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          disabled={isPending}
+          required
+        />
+      </div>
+
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {isPending ? <MiniSpinner className="mr-2" /> : null}
+        Sign In
+      </Button>
+    </form>
+  );
+}
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,8 +1,5 @@
-// src/pages/auth/Login.tsx
-import { useState } from "react";
-import { Button } from "@/ui/button";
-import { Input } from "@/ui/input";
-import { Label } from "@/ui/label";
+import { useUser } from "@/features/authentication/hooks/useUser";
+import LoginForm from "@/features/authentication/LoginForm";
 import {
   Card,
   CardContent,
@@ -10,27 +7,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/ui/card";
-import { useLogin } from "@/features/authentication/hooks/useLogin";
-import { MiniSpinner } from "@/ui/MiniSpinner";
+
 import { Logo } from "@/ui/Logo";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-  const [email, setEmail] = useState("oa5252842@gmail.com");
-  const [password, setPassword] = useState("omar011");
+  const { isAuthenticated } = useUser();
+  const navigate = useNavigate();
 
-  const { login, isPending } = useLogin();
-
-  function handleSubmit(e: React.SubmitEvent) {
-    e.preventDefault();
-    if (!email || !password) return;
-
-    login(
-      { email, password },
-      {
-        onSettled: () => setPassword(""),
-      },
-    );
-  }
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate("/admin/dashboard", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-50 p-4 transition-colors duration-300 dark:bg-[#0B1120]">
@@ -46,38 +36,7 @@ export default function Login() {
         </CardHeader>
 
         <CardContent className="space-y-8">
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="admin@example.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                disabled={isPending}
-                required
-              />
-            </div>
-
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                disabled={isPending}
-                required
-              />
-            </div>
-
-            <Button type="submit" className="w-full" disabled={isPending}>
-              {isPending ? <MiniSpinner className="mr-2" /> : null}
-              Sign In
-            </Button>
-          </form>
-
+          <LoginForm />
         </CardContent>
       </Card>
     </div>
